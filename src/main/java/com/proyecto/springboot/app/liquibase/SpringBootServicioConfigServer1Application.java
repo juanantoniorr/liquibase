0d634555d66adb79b83061e0d8115dbd258d1e17
juan <juan.antoniorr@hotmail.com>
2020-09-04 21:55:01 -0500

    first commit

---
+++ src/main/java/com/proyecto/springboot/app/liquibase/SpringBootServicioConfigServer1Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.springboot.app.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServicioConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioConfigServer1Application.class, args);
+	}
+
+}
